ef7c9f12925cc289f7525b4d0c5dc51d47df29ca
yui-szk <yuisuzuki.017@gmail.com>
2024-02-19 01:58:12 +0900

    入力欄を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import { BookList } from "@/components/BookList";
 
 export default function Page() {
   return (
     <div>
-      <div>Book-list</div>
-      <input type="text" />
+      <h1>Book-list</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("submit");
+        }}
+      >
+        <input type="text" placeholder="add book" />
+        <input type="date" />
+        <button type="submit">Add</button>
+      </form>
       <div>
         <BookList />
       </div>
