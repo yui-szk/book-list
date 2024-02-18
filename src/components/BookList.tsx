2d65255f81b7406d8bd788766e40a1658f383a3b
yui-szk <yuisuzuki.017@gmail.com>
2024-02-19 01:57:04 +0900

    BookItemをmap とりあえずのaddBook関数を追加

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,11 +1,51 @@
 "use client";
 
+import { useState } from "react";
 import { BookItem } from "./BookItem";
+import type { Book } from "@/types/book";
 
 export function BookList() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  function addBook() {
+    const newBook: Book = {
+      id: 1,
+      title: "",
+      readDate: new Date(),
+    };
+
+    setBooks((prevBooks) => [...prevBooks, newBook]);
+  }
+
   return (
-    <div>
-      <BookItem value="aaa" />
-    </div>
+    <ul>
+      {books.map((book) => (
+        <li key={book.id}>
+          <BookItem book={book} />
+        </li>
+      ))}
+    </ul>
   );
 }
+
+// dddのところに新しいbookを追加
+
+// MEMO
+// ジェネリクス
+// 型を定義する時に決まるようにする
+// useState
+
+// type Output<T> = (arg: T) => T;
+// // stringのときはstring, numberのときはnumberを返す
+// // string | numberだとstring->numberのときにエラーしない
+// const out: Output<String> = (arg) => {
+//   return "";
+// };
+
+// function out<T>(arg: T): T {
+//   return arg;
+// }
+// out(1); // 型推論
+// out("1");
+
+// out<string>(1); // error
